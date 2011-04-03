0f8f99abe058f7db2337edeede4fc23b65fc42c9
Ryo Asai <ryoasai74@gmail.com>
2011-04-03 12:38:46 +0900

    merge test

--- spring-mvc-exts-example/src/main/java/com/github/ryoasai/springmvc/example/flashmap/ResourceNotFoundException.java
+++ spring-mvc-exts-example/src/main/java/com/github/ryoasai/springmvc/example/flashmap/ResourceNotFoundException.java
@@ -8,7 +8,7 @@ public class ResourceNotFoundException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 
-	// test
+	// test3
 	private Long resourceId;
 	
 	public ResourceNotFoundException(Long resourceId) {
